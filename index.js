1a6ab505408908f550ac3e6a33cb3fc24cc32a5f
HuangjieJin <jinhuangjie233@gmail.com>
2021-03-14 20:43:01 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    for (let i=0; i< array.length; i++) {
+        array.splice(i, 1,`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return array;
+}
+
+function countDown(number){
+    for(let n=number; n>=0; n--){
+        console.log(n);
+    }
+    return number;
+}
+
